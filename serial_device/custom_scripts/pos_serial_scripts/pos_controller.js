e1d0553f6d628770522f25d7ad41f79d9097ab39
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-04-05 02:30:50 +0300

    Chagend locations of functions to have them called on item details initiation

--- serial_device/custom_scripts/pos_serial_scripts/pos_controller.js
+++ serial_device/custom_scripts/pos_serial_scripts/pos_controller.js
@@ -24,4 +24,72 @@ erpnext.PointOfSale.Controller = class extends erpnext.PointOfSale.Controller{
 			this.make_app();
 		});
 	}
+	
+	init_item_details() {
+		this.item_details = new erpnext.PointOfSale.ItemDetails({
+			wrapper: this.$components_wrapper,
+			settings: this.settings,
+			events: {
+				get_frm: () => this.frm,
+
+				toggle_item_selector: (minimize) => {
+					this.item_selector.resize_selector(minimize);
+					this.cart.toggle_numpad(minimize);
+				},
+
+				form_updated: async (cdt, cdn, fieldname, value) => {
+					const item_row = frappe.model.get_doc(cdt, cdn);
+					if (item_row && item_row[fieldname] != value) {
+
+						if (fieldname === 'qty' && flt(value) == 0) {
+							this.remove_item_from_cart();
+							return;
+						}
+
+						const { item_code, batch_no, uom } = this.item_details.current_item;
+						const event = {
+							field: fieldname,
+							value,
+							item: { item_code, batch_no, uom }
+						}
+						return this.on_cart_update(event)
+					}
+				},
+
+				item_field_focused: (fieldname) => {
+					this.cart.toggle_numpad_field_edit(fieldname);
+				},
+				set_value_in_current_cart_item: (selector, value) => {
+					this.cart.update_selector_value_in_cart_item(selector, value, this.item_details.current_item);
+				},
+				clone_new_batch_item_in_frm: (batch_serial_map, current_item) => {
+					// called if serial nos are 'auto_selected' and if those serial nos belongs to multiple batches
+					// for each unique batch new item row is added in the form & cart
+					Object.keys(batch_serial_map).forEach(batch => {
+						const { item_code, batch_no } = current_item;
+						const item_to_clone = this.frm.doc.items.find(i => i.item_code === item_code && i.batch_no === batch_no);
+						const new_row = this.frm.add_child("items", { ...item_to_clone });
+						// update new serialno and batch
+						new_row.batch_no = batch;
+						new_row.serial_no = batch_serial_map[batch].join(`\n`);
+						new_row.qty = batch_serial_map[batch].length;
+						this.frm.doc.items.forEach(row => {
+							if (item_code === row.item_code) {
+								this.update_cart_html(row);
+							}
+						});
+					})
+				},
+				remove_item_from_cart: () => this.remove_item_from_cart(),
+				get_item_stock_map: () => this.item_stock_map,
+				close_item_details: () => {
+					this.item_details.toggle_item_details_section(undefined);
+					this.cart.prev_action = undefined;
+					this.cart.toggle_item_highlight();
+					window.is_item_details_open = false;
+				},
+				get_available_stock: (item_code, warehouse) => this.get_available_stock(item_code, warehouse)
+			}
+		});
+	}
 }
--- serial_device/serial_device/page/point_of_sale/point_of_sale.js
+++ serial_device/serial_device/page/point_of_sale/point_of_sale.js
@@ -8,13 +8,100 @@ frappe.pages['point-of-sale'].on_page_load = function(wrapper) {
 	});
 
 	frappe.require('assets/js/point-of-sale.min.js', function() {
-		//window.extensionId = "lhphimlabkfchifcfjagjihkjpdcgdgi";
 		window.serialPort = new erpnext.PointOfSale.SerialPort();		
 		wrapper.pos = new erpnext.PointOfSale.Controller(wrapper);
 		window.cur_pos = wrapper.pos;
-		setTimeout(function(){
-			console.log({"wrapper": frappe.pages['point-of-sale']});
-		}, 5000);
+		
+		//Initialize connection with serial device
+		/*window.serialPort.setOnDataReceivedCallback(window.onNewData);
+		window.port = chrome.runtime.connect(window.extensionId);
+		window.add_listener();*/
+		window.is_item_details_open = false;
+		window.add_listener = function(){
+			window.port.onMessage.addListener(
+				function(msg) {
+				  console.log({"msg":msg});
+				  if(msg.header === "guid"){
+					window.portGUID = msg.guid;
+					window.openSelectedPort();
+				  }
+				  else if(msg.header === "serialdata"){
+					window.onNewData(new Uint8Array(msg.data).buffer);
+					/*if(onDataReceivedCallback !== undefined){
+					  onDataReceivedCallback(new Uint8Array(msg.data).buffer);
+					}*/
+				  }
+				  else if(msg.header === "serialerror"){
+					window.onErrorReceivedCallback(msg.error);
+				  }
+				}
+			);
+		}
+		
+		window.sendData = function(){
+			var input = window.stringToArrayBuffer("W");
+
+			window.serialPort.write(input,
+			  function(response){
+				console.log(response);
+			  }
+			);
+		}
+
+		window.stringToArrayBuffer = function(string){
+			var buffer = new ArrayBuffer(string.length);
+			var dv = new DataView(buffer);
+			for(var i = 0; i < string.length; i++){
+			  dv.setUint8(i, string.charCodeAt(i));
+			}
+			return dv.buffer;
+		}
+		
+		window.openSelectedPort = function(){
+			window.serialPort.openPort(
+			  {
+				portName: window.portName,
+				bitrate: window.bitrate,
+				dataBits: window.dataBits,
+				parityBit: window.parityBit,
+				stopBits: window.stopBits
+			  },
+			  window.portGUID,
+			  function(response){
+				console.log(response);
+				if(response.result === "ok"){
+				  //Do something
+				  alert("Weigh device connected!");
+				}
+				else{
+				  alert(response.error);
+				}
+			  }
+			);
+		}
+		
+		window.onNewData = function(data){
+			var str = "";
+			var dv = new DataView(data);
+			for(var i = 0; i < dv.byteLength; i++){
+				str = str.concat(String.fromCharCode(dv.getUint8(i, true)));
+			}
+			var weight = parseFloat(str)
+			console.log(weight);
+			if(isNaN(weight)){
+				//setTimeout(this.sendData(), 500);
+			}
+			else{ 
+				if(weight > 0 && weight != window.old_weight){
+					window.old_weight = weight;
+					window.qty_control.set_value(weight);
+					
+					//$('qty_control').value(weight);
+					//$('#output').append(weight);
+				}
+				setTimeout(window.sendData(), 200);
+			}
+		}
 	});
 };
 
